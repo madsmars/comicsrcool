573e01db4f63665f5ef3e92de256b05cea576208
madsmars <mtheli@saic.edu>
2023-03-30 20:31:19 -0500

    i hoesntly cant tell you what i did. it just came to me like a fever dream. but i did it and it's a good example of the gallery option. pls i want this job i hope they like it. cries

---
+++ main.js
@@ -0,0 +1,103 @@
+//dark mode start
+// function toggleDarkMode() {
+//   var element = document.body;
+//   element.classList.toggle("dark-mode");
+// }
+
+// GALLERY START
+displayGalleryOnLoad();
+
+function displayGalleryOnLoad() {
+    
+    const URL = window.location.href.split('#');
+    console.log(window.location.href)
+
+    // checks if the default page should be loaded
+    if(URL.length == 1) {
+        return;
+    }
+
+    const selectedGallery = "#" + URL[URL.length - 1];
+    $(`${selectedGallery}`).show();
+    $(".slideshow-container").not(`${selectedGallery}`).hide();
+}
+
+// any time one of the .gallery-links is clicked...
+$('.main-nav1').click(function () {
+
+    console.log("hello")
+
+    const selectedGallery = $(this).attr('href');
+    console.log(selectedGallery)
+
+    $(`${selectedGallery}`).show();
+    $(".slideshow-container").not(`${selectedGallery}`).hide();
+})
+
+// function generateAllImages() {
+//     const mainImage = $(".gallery a img").parent()
+//     const allGallery = $("#all .gallery")
+    
+//     for (let i = 0; i < mainImage.length; i++) {
+//         allGallery.append(mainImage[i])
+//     }
+// }
+
+// generateAllImages()
+// GALLERY END
+
+// SLIDESHOW START
+// let slideIndex = 1;
+// showSlides(slideIndex);
+
+// // Next/previous controls
+// function plusSlides(n) {
+//   showSlides(slideIndex += n);
+// }
+
+// // Thumbnail image controls
+// function currentSlide(n) {
+//   showSlides(slideIndex = n);
+// }
+
+// function showSlides(n) {
+//   let i;
+//   let slides = document.getElementsByClassName("mySlides");
+//   let dots = document.getElementsByClassName("dot");
+//   if (n > slides.length) {slideIndex = 1}
+//   if (n < 1) {slideIndex = slides.length}
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";
+//   }
+//   for (i = 0; i < dots.length; i++) {
+//     dots[i].className = dots[i].className.replace(" active", "");
+//   }
+//   slides[slideIndex-1].style.display = "block";
+//   dots[slideIndex-1].className += " active";
+// }
+// // SLIDESHOW END
+
+//MULTIPLE START
+let slideIndex = [1,1];
+/* Class the members of each slideshow group with different CSS classes */
+let slideId = ["mySlides1", "mySlides2",]
+showSlides(1, 0);
+showSlides(1, 1);
+
+
+
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
+}
+
+function showSlides(n, no) {
+  let i;
+  let x = document.getElementsByClassName(slideId[no]);
+  if (n > x.length) {slideIndex[no] = 1}
+  if (n < 1) {slideIndex[no] = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex[no]-1].style.display = "block";
+}
+//MULTIPLE END
